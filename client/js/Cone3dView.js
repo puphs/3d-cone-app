93eb9a3932a61bf20ed7bfc7b70539ac3aa58256
puphs <43395605+puphs@users.noreply.github.com>
2021-01-08 16:39:13 +0400

    create client javascript

---
+++ client/js/Cone3dView.js
@@ -0,0 +1,80 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+
+export default class Cone3dView {
+	constructor(viewParentSelector) {
+		const parent = document.querySelector(viewParentSelector);
+
+		this.scene = new THREE.Scene();
+		this.camera = new THREE.PerspectiveCamera(
+			75,
+			parent.offsetWidth / parent.offsetHeight,
+			0.1,
+			1000
+		);
+		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer.setClearColor(0xdddddd);
+		this.renderer.setSize(parent.offsetWidth, parent.offsetHeight);
+
+		this.camera.position.set(0, 2, 5.5);
+
+		// setup light
+		const light = new THREE.PointLight(0xffffff, 1, 1000);
+		light.position.set(50, 50, 50);
+		this.scene.add(light);
+
+		// setup orbit controls (camera moving, rotating and zoom)
+		this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+		this.orbitControls.update();
+		this.orbitControls.enableDamping = true;
+		this.orbitControls.dampingFactor = 0.15;
+		this.orbitControls.enableZoom = true;
+		// this.scene.add(new THREE.AxesHelper(500));
+
+		// add renderer to dom
+		parent.append(this.renderer.domElement);
+		// set up renderer size on window resize
+		window.addEventListener('resize', () => {
+			this.renderer.setSize(parent.offsetWidth, parent.offsetHeight);
+			this.camera.aspect = parent.offsetWidth / parent.offsetHeight;
+			this.camera.updateProjectionMatrix();
+			this.render();
+		});
+
+		// create cone material, geometry and mesh
+		this.coneGeometry = new THREE.Geometry();
+		this.coneMaterial = new THREE.MeshLambertMaterial({ color: 0xdddddd, side: THREE.DoubleSide });
+		this.coneMesh = new THREE.Mesh(this.coneGeometry, this.coneMaterial);
+		this.coneMesh.rotation.x = -Math.PI / 2;
+
+		this.scene.add(this.coneMesh);
+
+		this.animate();
+	}
+
+	drawCone(triangles) {
+		this.coneGeometry.dispose();
+		this.coneGeometry = new THREE.Geometry();
+		triangles.forEach((triangle, index) => {
+			triangle.forEach((point) => {
+				const vertice = new THREE.Vector3(point.x, point.y, point.z);
+				this.coneGeometry.vertices.push(vertice);
+			});
+			const face = new THREE.Face3(index * 3, index * 3 + 1, index * 3 + 2);
+			this.coneGeometry.faces.push(face);
+		});
+		this.coneGeometry.computeFaceNormals();
+
+		this.coneMesh.geometry = this.coneGeometry;
+	}
+
+	render() {
+		this.renderer.render(this.scene, this.camera);
+	}
+
+	animate() {
+		requestAnimationFrame(() => this.animate());
+		this.orbitControls.update();
+		this.render();
+	}
+}
--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,60 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import Toast from './Toast.js';
+import Cone3dView from './Cone3dView.js';
+
+const CONE_TRIANGLES_API_URL = document.location.origin + '/api/cone-triangles';
+
+const API_RESULT_CODE = {
+	OK: 0,
+	ERROR: 1,
+};
+
+const parseForm = (form) => {
+	const formData = new FormData(form);
+	const formBody = {};
+	for (let [key, value] of formData) {
+		formBody[key] = value;
+	}
+	return formBody;
+};
+
+const drawConeAndHandleErrors = async (height, radius, segmentsCount) => {
+	fetchConeTriangles(height, radius, segmentsCount)
+		.then((response) => {
+			if (response.resultCode == API_RESULT_CODE.OK) {
+				cone3dView.drawCone(response.data);
+			} else {
+				errorToast.setMessage(response.messages[0]);
+				errorToast.show();
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
+const fetchConeTriangles = (height, radius, segmentsCount) => {
+	const body = { height, radius, segmentsCount };
+	return fetch(CONE_TRIANGLES_API_URL, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: 'POST',
+		body: JSON.stringify(body),
+	}).then((data) => data.json());
+};
+
+const errorToast = new Toast(Toast.DURATION_SHORT);
+const cone3dView = new Cone3dView('.view-3d');
+const coneSetupForm = document.querySelector('.cone-setup-form');
+
+// draw cone with default values when user enters the page
+drawConeAndHandleErrors(2, 1, 6);
+
+coneSetupForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const { height, radius, segmentsCount } = parseForm(coneSetupForm);
+	drawConeAndHandleErrors(height, radius, segmentsCount);
+
+	return false;
+});
