da4b08f84bcee5c4ca1f76c6678736b22e1ef69f
puphs <43395605+puphs@users.noreply.github.com>
2021-01-17 13:48:15 +0400

    create optimized cone triangulation alghoritm

--- cone.js
+++ cone.js
@@ -29,12 +29,44 @@ const calculateConeTriangles = (height, radius, segmentsCount) => {
 /* 
     returns an object with the x, y, and z coordinates of the point lying on the radius of the circle 
 */
-const getPointOnCircleRadius = (radius, segmentIndex, segmentsCount) => {
-	return {
-		x: radius * Math.cos((2 * Math.PI * segmentIndex) / segmentsCount),
-		y: radius * Math.sin((2 * Math.PI * segmentIndex) / segmentsCount),
-		z: 0,
-	};
+const getPointOnCircleRadius = (radius, segmentIndex, segmentsCount) =>
+	point(
+		radius * Math.cos((2 * Math.PI * segmentIndex) / segmentsCount),
+		radius * Math.sin((2 * Math.PI * segmentIndex) / segmentsCount),
+		0
+	);
+
+const point = (x, y, z) => ({ x, y, z });
+
+/* 
+	returns an object with cone points and cone trianglesIndexes (array of 3 points indexes)
+*/
+const calculateConeGeometry = (height, radius, segmentsCount) => {
+	const points = [];
+	const trianglesIndexes = [];
+	const geometry = { points, trianglesIndexes };
+
+	// top point with index = 0
+	const topPoint = point(0, 0, height);
+	points.push(topPoint);
+
+	let previousPoint = getPointOnCircleRadius(radius, 0, segmentsCount);
+	points.push(previousPoint);
+
+	let i = 1;
+	while (i <= segmentsCount) {
+		const triangleIndexes = [0, i + 1, i];
+
+		const currentPoint = getPointOnCircleRadius(radius, i, segmentsCount);
+		points.push(currentPoint);
+
+		previousPoint = currentPoint;
+
+		trianglesIndexes.push(triangleIndexes);
+		i++;
+	}
+
+	return geometry;
 };
 
-module.exports = { calculateConeTriangles };
+module.exports = { calculateConeTriangles, calculateConeGeometry };
